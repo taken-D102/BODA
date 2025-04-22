52a90fb3e1b413eb48bc4c5d6cf1a8d6952f9c65
정지원 <littlesam95@naver.com>
2025-04-22 17:28:07 +0900

    design: 로고 변경, Color, Theme 변경

---
+++ Android/Boda/app/src/main/java/com/taken/boda/utils/ContextExtension.kt
@@ -0,0 +1,10 @@
+package com.taken.boda.utils
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+fun Context.hasPermission(permissionType: String): Boolean {
+    return ContextCompat.checkSelfPermission(this, permissionType) ==
+            PackageManager.PERMISSION_GRANTED
+}
